5b7964659b19c50713ff6ff1ca7f6b99d68d299a
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-06-13 12:45:52 -0500

    Prevent dirt from being placed on top of soil

--- Source/Harmony.cs
+++ Source/Harmony.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using Verse;
 
 namespace TilledSoil
@@ -43,6 +45,61 @@ namespace TilledSoil
             {
                 Utilities.DestroyPlantsAt(c, ___map);
             }
+        }
+    }
+
+    //Prevent dirt from being placed on top of soil
+    [HarmonyPatch(typeof(GenConstruct), nameof(GenConstruct.CanPlaceBlueprintAt))]
+    public static class GenConstruct_CanPlaceBlueprintAt
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            CodeMatcher matcher = new(instructions, generator);
+            matcher.DeclareLocal(typeof(TerrainDef), out LocalBuilder existingTerrainLocal);
+            // Find the call to TerrainAt
+            matcher.MatchStartForward([
+                    new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(TerrainGrid), nameof(TerrainGrid.TerrainAt), [typeof(IntVec3)])),
+                    new CodeMatch(OpCodes.Ldloc_3),
+                    new CodeMatch(OpCodes.Bne_Un_S)]
+                );
+            matcher.InsertAfterAndAdvance(
+                //Store the existing terrain as a local variable
+                new CodeInstruction(OpCodes.Stloc, existingTerrainLocal),
+                //Put it back on the stack
+                new CodeInstruction(OpCodes.Ldloc, existingTerrainLocal.LocalIndex)
+                );
+            matcher.Advance(1);
+            //Copy the instruction for loading the new terrain on the stack
+            CodeInstruction newterrain = matcher.Instruction;
+            matcher.Advance(1);
+            //Put a label on the original branch
+            matcher.CreateLabel(out Label branchLabel);
+            //Put the terrains on the stack again
+            matcher.InsertAndAdvance(
+                new CodeInstruction(OpCodes.Ldloc, existingTerrainLocal.LocalIndex),
+                new CodeInstruction(newterrain.opcode, newterrain.operand)
+                );
+
+            //Call the helper
+            matcher.Insert(
+                CodeInstruction.Call(typeof(GenConstruct_CanPlaceBlueprintAt), nameof(Intercept)),
+                //Jump to the original branch if the helper returns false
+                new CodeInstruction(OpCodes.Brfalse_S, branchLabel),
+                //If true, which means it's my special case, remove new terrain from the stack and add existing, so that existing existing is on the stack
+                //Then the original branch will be see them as the same
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Ldloc, existingTerrainLocal.LocalIndex)
+                );
+
+            return matcher.InstructionEnumeration();
+        }
+        public static bool Intercept(TerrainDef existing, TerrainDef newTerrain)
+        {
+            if (newTerrain == DefOfTS.Dirt && existing == TerrainDefOf.Soil)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
